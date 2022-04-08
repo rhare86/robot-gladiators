58798b011e7e7a15a65d7465380f1f4923d2cf5e
rhare86 <rhare86@gmail.com>
2022-04-08 18:33:23 -0500

    fixed player name requirement

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,8 +2,19 @@ var randomNumber = function(min , max) {
     var value = Math.floor(Math.random() * (max - min +1) + min);
     return value;
 };
+var getPlayerName = function() {
+    var name = "";
+while (name === "" || name === null){
+    name = prompt("What is your robot's name?");
+};
+
+
+console.log("Your robot's name is " + name);
+return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100, 
     attack: 10,
     money:10,
